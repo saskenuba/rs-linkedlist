3a806fd825c2aa328432ff6e733a8f9b98f60338
Martin <martin@hotmail.com.br>
2018-12-22 20:40:52 -0200

    added close to pop method, fixed new function on List impl

--- src/second.rs
+++ src/second.rs
@@ -1,13 +1,11 @@
 // An Ok Singly-Linked Stack
 
-use std::mem;
-
 pub struct List {
     head: Link,
 }
 
 // introducing type aliases!
-type Link = Option<Box<Node>>
+type Link = Option<Box<Node>>;
 
 struct Node {
     elem: i32,
@@ -16,14 +14,14 @@ struct Node {
 
 impl List {
     pub fn new() -> Self {
-        List { head: Link::Empty }
+        List { head: None }
     }
 
     pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem: elem,
             // changing std::replace, for the Option method take
-            next: self.head.take()
+            next: self.head.take(),
         });
 
         self.head = Some(new_node)
@@ -31,21 +29,17 @@ impl List {
 
     #[allow(unused_variables)]
     pub fn pop(&mut self) -> Option<i32> {
-        // here we need mem::replace because Rust by default move the value they match
-        match self.head.take() {
-            None => None,
-            Some(node) => {
-                let node = *node;
-                self.head = node.next;
-                Some(node.elem)
-            }
-        }
+        self.head.take().map(|node| {
+            let node = *node;
+            self.head = node.next;
+            node.elem
+        })
     }
 }
 
 impl Drop for List {
     fn drop(&mut self) {
-        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        let mut cur_link = self.head.take();
         while let Some(mut boxed_node) = cur_link {
             cur_link = boxed_node.next.take();
         }
@@ -82,7 +76,5 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
-
-
     }
 }
