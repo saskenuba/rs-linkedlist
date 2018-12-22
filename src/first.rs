0ce153806d78f4736a34e3fb4247df86a2d43cef
Martin <martin@hotmail.com.br>
2018-12-22 17:51:35 -0200

    fixed push, added pop method, and also some tests

--- src/first.rs
+++ src/first.rs
@@ -20,13 +20,57 @@ impl List {
     }
 
     pub fn push(&mut self, elem: i32) {
-        let new_node = Node {
+        let new_node = Box::new(Node {
             elem: elem,
-            // here rust notices when self.head moves to next, and rust doesnt like
-            // that we leave it partially initialized untill we give the value back.
-            next: self.head,
-        };
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
 
         self.head = Link::More(new_node);
+    }
+
+    #[allow(unused_variables)]
+    pub fn pop(&mut self) -> Option<i32> {
+        // here we need mem::replace because Rust by default move the value they match
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(boxed_node) => {
+                let node = *boxed_node;
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+// Added some tests
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check removals
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
     }
 }
